256fb4b47bef6b1063b296fef0ac09f542f35e96
Code-Apprentice <codeguru42@gmail.com>
2023-10-14 17:51:03 -0600

    Add many-to-many relationships

--- api/api/models.py
+++ api/api/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 
+class Resource(models.Model):
+    name = models.CharField(max_length=255)
+    abbreviation = models.CharField(max_length=255)
+
+
 class Star(models.Model):
     name = models.CharField(max_length=255)
     level = models.IntegerField
@@ -10,14 +15,11 @@ class Planet(models.Model):
     name = models.CharField(max_length=255)
     star = models.ForeignKey(Star, on_delete=models.PROTECT, null=False)
     extreme_environment = models.BooleanField()
+    resources = models.ManyToManyField(Resource)
 
 
 class Moon(models.Model):
     name = models.CharField(max_length=255)
     planet = models.ForeignKey(Planet, on_delete=models.PROTECT, null=False)
     extreme_environment = models.BooleanField()
-
-
-class Resource(models.Model):
-    name = models.CharField(max_length=255)
-    abbreviation = models.CharField(max_length=255)
+    resources = models.ManyToManyField(Resource)
