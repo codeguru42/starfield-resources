9e70ecc6ac696beafda88c1e6429a8bdebf781df
Code-Apprentice <codeguru42@gmail.com>
2023-10-14 17:46:06 -0600

    More models

--- api/api/models.py
+++ api/api/models.py
@@ -4,3 +4,20 @@ from django.db import models
 class Star(models.Model):
     name = models.CharField(max_length=255)
     level = models.IntegerField
+
+
+class Planet(models.Model):
+    name = models.CharField(max_length=255)
+    star = models.ForeignKey(Star, on_delete=models.PROTECT, null=False)
+    extreme_environment = models.BooleanField()
+
+
+class Moon(models.Model):
+    name = models.CharField(max_length=255)
+    planet = models.ForeignKey(Planet, on_delete=models.PROTECT, null=False)
+    extreme_environment = models.BooleanField()
+
+
+class Resource(models.Model):
+    name = models.CharField(max_length=255)
+    abbreviation = models.CharField(max_length=255)
