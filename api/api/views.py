4cf7ecdd8c071fe2d734badf9ae41ffffe2f7dad
Code-Apprentice <codeguru42@gmail.com>
2023-10-14 18:02:41 -0600

    Views

--- api/api/views.py
+++ api/api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from api import models, serializers
+
+
+class ResourceViewSet(viewsets.ModelViewSet):
+    queryset = models.Resource.objects.all()
+    serializer_class = serializers.ResourceSerializer
+
+
+class StarViewSet(viewsets.ModelViewSet):
+    queryset = models.Star.objects.all()
+    serializer_class = serializers.StarSerializer
+
+
+class PlanetViewSet(viewsets.ModelViewSet):
+    queryset = models.Planet.objects.all()
+    serializer_class = serializers.PlanetSerializer
+
+
+class MoonViewSet(viewsets.ModelViewSet):
+    queryset = models.Moon.objects.all()
+    serializer_class = serializers.MoonSerializer
