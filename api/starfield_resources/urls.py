7194be43c00acff415741128badf01088e330d11
Code-Apprentice <codeguru42@gmail.com>
2023-10-14 22:25:40 -0600

    Token authentication

--- api/starfield_resources/urls.py
+++ api/starfield_resources/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from rest_framework.authtoken import views as auth_views
 
 from starfield_resources import views
 
@@ -25,4 +25,5 @@ urlpatterns = [
     path("api/", include("api.urls")),
     path("health_check/", views.health_check),
     path("auth/", include("rest_framework.urls")),
+    path("auth/token/", auth_views.obtain_auth_token),
 ]
