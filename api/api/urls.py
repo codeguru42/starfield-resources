a8f87f82bd82d88b557523d67ec567c1c4d275ab
Code-Apprentice <codeguru42@gmail.com>
2023-10-14 18:23:37 -0600

    Swagger docs

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from django.views.generic import TemplateView
 from rest_framework import routers
+from rest_framework.schemas import get_schema_view
 
 from api import views
 
@@ -9,4 +11,23 @@ router.register("stars", views.StarViewSet)
 router.register("planets", views.PlanetViewSet)
 router.register("moons", views.MoonViewSet)
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("", include(router.urls)),
+    path(
+        "schema",
+        get_schema_view(
+            title="Starfield Resources",
+            description="REST API to search for resources in Starfield",
+            version="1.0.0",
+        ),
+        name="openapi-schema",
+    ),
+    path(
+        "docs",
+        TemplateView.as_view(
+            template_name="swagger.html",
+            extra_context={"schema_url": "openapi-schema"},
+        ),
+        name="docs",
+    ),
+]
