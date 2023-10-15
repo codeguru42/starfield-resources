e5d1e738bd683d5bd8831da67139250cad04b1b8
Code-Apprentice <codeguru42@gmail.com>
2023-10-15 05:02:18 -0600

    Data grid

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,25 @@
 import './App.css'
+import {DataGrid, GridColDef} from "@mui/x-data-grid";
+import {planets} from "./data.ts";
 
 function App() {
+  const columns: GridColDef[] = [
+    {
+      field: 'name',
+      headerName: 'Planet',
+      width: 150,
+    },
+    {
+      field: 'star',
+      headerName: 'Star',
+      width: 150,
+    }
+  ]
+
   return (
     <>
       <h1>Starfield Resources</h1>
+      <DataGrid columns={columns} rows={planets}/>
     </>
   )
 }
