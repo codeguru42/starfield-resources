adcfae67b890dd8a84c4270d03cf277aeff46312
Code-Apprentice <codeguru42@gmail.com>
2023-10-14 19:57:16 -0600

    Health check

--- api/starfield_resources/urls.py
+++ api/starfield_resources/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 import api
+from starfield_resources import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
+    path("health_check/", views.health_check),
 ]
