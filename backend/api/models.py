6a2c0637c9a347317ba9d117e1dc41dc571b815b
Code-Apprentice <codeguru42@gmail.com>
2023-10-15 23:21:57 -0600

    Rename related fields

--- backend/api/models.py
+++ backend/api/models.py
@@ -13,13 +13,17 @@ class Star(models.Model):
 
 class Planet(models.Model):
     name = models.CharField(max_length=255)
-    star = models.ForeignKey(Star, on_delete=models.PROTECT, null=False)
+    star = models.ForeignKey(
+        Star, on_delete=models.PROTECT, null=False, related_name="planets"
+    )
     extreme_environment = models.BooleanField()
     resources = models.ManyToManyField(Resource)
 
 
 class Moon(models.Model):
     name = models.CharField(max_length=255)
-    planet = models.ForeignKey(Planet, on_delete=models.PROTECT, null=False)
+    planet = models.ForeignKey(
+        Planet, on_delete=models.PROTECT, null=False, related_name="moons"
+    )
     extreme_environment = models.BooleanField()
     resources = models.ManyToManyField(Resource)
