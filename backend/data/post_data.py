b65ae74d081c59b8386c4571fbdc2d67737417a0
Code-Apprentice <codeguru42@gmail.com>
2023-10-16 00:46:43 -0600

    Start parsing csv file with planet data

---
+++ backend/data/post_data.py
@@ -0,0 +1,21 @@
+import math
+
+import pandas as pd
+import typer
+
+
+def get_resources(planet_data):
+    resource_cols = [f"Resource {i+1}" for i in range(8)]
+    all_resources = pd.concat(
+        planet_data[resource_col] for resource_col in resource_cols
+    )
+    return all_resources[~all_resources.isna()].unique()
+
+
+def main(filename: str):
+    planet_data = pd.read_csv(filename)
+    print(get_resources(planet_data))
+
+
+if __name__ == "__main__":
+    typer.run(main)
