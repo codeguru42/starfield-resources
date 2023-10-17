bcd06d7fca83cc214c4fff490e952d6feb7304aa
Code-Apprentice <codeguru42@gmail.com>
2023-10-16 21:16:58 -0600

    Client for auth token route

---
+++ backend/client/auth.py
@@ -0,0 +1,14 @@
+import requests
+
+
+class AuthClient:
+    TOKEN_ROUTE = "http://localhost:8000/auth/token/"
+
+    def token(self, username, passsword):
+        # TODO: Error handling
+        body = {
+            "username": username,
+            "password": passsword,
+        }
+        response = requests.post(AuthClient.TOKEN_ROUTE, data=body)
+        return response.json()
