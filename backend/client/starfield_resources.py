a8a0861245cd24777cd519e65e78f80f016368ce
Code-Apprentice <codeguru42@gmail.com>
2023-10-16 22:01:29 -0600

    Insert resources by posting to API

---
+++ backend/client/starfield_resources.py
@@ -0,0 +1,18 @@
+import requests
+
+from client import models
+
+
+class ApiClient:
+    RESOURCE_ROUTE = "http://localhost:8000/api/resources"
+
+    def __init__(self, token: models.Token):
+        self.headers = {
+            "Authorization": f"Token {token.token}",
+            "Content-Type": "application/json",
+        }
+
+    def post_resource(self, resource: models.Resource):
+        # TODO: Error handling
+        body = resource.model_dump_json()
+        requests.post(ApiClient.RESOURCE_ROUTE, headers=self.headers, data=body)
--- backend/data/post_data.py
+++ backend/data/post_data.py
@@ -1,6 +1,11 @@
 import numpy as np
 import pandas as pd
 import typer
+from pydantic import ValidationError
+
+from client import models
+from client.auth import AuthClient
+from client.starfield_resources import ApiClient
 
 app = typer.Typer()
 
@@ -22,6 +27,19 @@ def parse_resources(filename: str):
     planet_data = pd.read_csv(filename)
     resources = get_resources(planet_data)
     write_resources(resources)
+
+
+@app.command()
+def post_resources(filename: str, username: str, password: str):
+    token = AuthClient().token(username, password)
+    resource_data = pd.read_csv(filename)
+    resource_data.columns = map(str.lower, resource_data.columns)
+    client = ApiClient(token)
+    for resource in resource_data.to_dict("records"):
+        try:
+            client.post_resource(models.Resource(**resource))
+        except ValidationError:
+            print(f"Unable to insert: {resource}")
 
 
 if __name__ == "__main__":
