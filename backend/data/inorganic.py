483b36e322379114960151d63cdeac2e2c6344f6
Code-Apprentice <codeguru42@gmail.com>
2023-10-23 15:44:35 -0600

    Move helper functions to separate source file

---
+++ backend/data/inorganic.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from pydantic import ValidationError
+
+from client import models
+from client.auth import AuthClient
+from client.starfield_resources import ApiClient
+
+
+def _extract_inorganic(filename):
+    return pd.read_csv(filename)
+
+
+def _transform_inorganic(resource_data):
+    resource_data.columns = map(str.lower, resource_data.columns)
+    resource_data = resource_data[["resource", "description", "rarity"]]
+    resource_data = resource_data.rename(
+        columns={"description": "name", "resource": "abbreviation"}
+    )
+    return resource_data
+
+
+def _load_inorganic(resource_data, username, password):
+    try:
+        token = AuthClient().token(username, password)
+        client = ApiClient(token)
+        for resource in resource_data.to_dict("records")[1:]:
+            try:
+                resource["rarity"] = models.Rarity[resource["rarity"].upper()]
+                model = models.Resource(**resource)
+                response = client.post_resource(model)
+                # TODO should post_resource() do this check and throw an
+                #  exception instead?
+                if response.status_code != 201:
+                    print(model)
+                    print(response.json())
+            except ValidationError as err:
+                print(f"Unable to insert: {resource}")
+                print(err.json())
+    except ValidationError as err:
+        print(err.json())
--- backend/data/load_data.py
+++ backend/data/load_data.py
@@ -6,6 +6,7 @@ from pydantic import ValidationError
 from client import models
 from client.auth import AuthClient
 from client.starfield_resources import ApiClient
+from data.inorganic import _extract_inorganic, _transform_inorganic, _load_inorganic
 
 app = typer.Typer()
 
@@ -34,40 +35,6 @@ def load_inorganic(filename: str, username: str, password: str):
     resource_data = _extract_inorganic(filename)
     resource_data = _transform_inorganic(resource_data)
     _load_inorganic(resource_data, username, password)
-
-
-def _load_inorganic(resource_data, username, password):
-    try:
-        token = AuthClient().token(username, password)
-        client = ApiClient(token)
-        for resource in resource_data.to_dict("records")[1:]:
-            try:
-                resource["rarity"] = models.Rarity[resource["rarity"].upper()]
-                model = models.Resource(**resource)
-                response = client.post_resource(model)
-                # TODO should post_resource() do this check and throw an
-                #  exception instead?
-                if response.status_code != 201:
-                    print(model)
-                    print(response.json())
-            except ValidationError as err:
-                print(f"Unable to insert: {resource}")
-                print(err.json())
-    except ValidationError as err:
-        print(err.json())
-
-
-def _transform_inorganic(resource_data):
-    resource_data.columns = map(str.lower, resource_data.columns)
-    resource_data = resource_data[["resource", "description", "rarity"]]
-    resource_data = resource_data.rename(
-        columns={"description": "name", "resource": "abbreviation"}
-    )
-    return resource_data
-
-
-def _extract_inorganic(filename):
-    return pd.read_csv(filename)
 
 
 if __name__ == "__main__":
